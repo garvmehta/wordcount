e86fcfab1731bbca73f75e6216ea448cd0d5c368
garvmehta <63910721+garvmehta@users.noreply.github.com>
2020-08-19 16:21:43 +0530

    Updates

---
+++ precache-manifest.2496826ffd6ffd2b1454591dcfecd86c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77157c12f213d2bc56550f147d3cc2c5",
+    "url": "/wordcount/index.html"
+  },
+  {
+    "revision": "8d260b06d05bdaae28a6",
+    "url": "/wordcount/static/css/main.f30bd6bb.chunk.css"
+  },
+  {
+    "revision": "e2e327135fc4b5d1745b",
+    "url": "/wordcount/static/js/2.ae06b18e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wordcount/static/js/2.ae06b18e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d260b06d05bdaae28a6",
+    "url": "/wordcount/static/js/main.dbb61508.chunk.js"
+  },
+  {
+    "revision": "e11f6c811663fd20c9ea",
+    "url": "/wordcount/static/js/runtime-main.ea16aecc.js"
+  }
+]);
